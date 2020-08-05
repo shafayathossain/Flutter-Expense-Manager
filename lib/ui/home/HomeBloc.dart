2b50cea3603b3c50f160389eb8e27090e03eddaa
Tanvir Ahmod <tanvirahmodcse@gmail.com>
2020-08-05 18:39:26 +0600

    Fix GetThisMonthBalanceEvent issue

--- lib/ui/home/HomeBloc.dart
+++ lib/ui/home/HomeBloc.dart
@@ -7,54 +7,52 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rxdart/rxdart.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-
   HomeRepository _repository;
   PublishSubject<List<WalletWithBalance>> wallets = PublishSubject();
-  PublishSubject<List<double>> incomeAndExpenses = PublishSubject()..add([0, 0]);
+  PublishSubject<List<double>> incomeAndExpenses = PublishSubject()
+    ..add([0, 0]);
 
-  HomeBloc(this._repository): super(null);
+  HomeBloc(this._repository) : super(null);
 
   @override
   Stream<HomeState> mapEventToState(HomeEvent event) async* {
     print("-------> $event");
-    if(event is GetWalletsEvent) {
+    if (event is GetWalletsEvent) {
       yield* _getWalletsWithBalance();
-    } else if(event is GetThisMonthBalanceEvent) {
+    } else if (event is GetThisMonthBalanceEvent) {
       yield* _getCashFlow(event.startTime, event.endTime);
-    } else if(event is InitialEvent) {
+    } else if (event is InitialEvent) {
       yield* Stream.value(InitState());
-    } else if(event is ResetEvent) {
+    } else if (event is ResetEvent) {
       yield* Stream.value(ResetState());
     }
   }
 
   Stream<HomeState> _getWalletsWithBalance() async* {
-    yield* _repository.getWalletsWithBalance().map((event) {
+    _repository.getWalletsWithBalance().map((event) {
       wallets.sink.add(event.map((e) {
-        double percent = e.income > 0.0 ? (e.balance.abs() / e.income.abs()) : 0;
+        double percent =
+            e.income > 0.0 ? (e.balance.abs() / e.income.abs()) : 0;
         print("PERCENT $percent");
-        return (e..balancePercent = percent);
+        e..balancePercent = percent;
       }).toList());
-      return HomeState();
     });
+    yield HomeState();
   }
 
   Stream<HomeState> _getCashFlow(int startTime, int endTime) async* {
-    yield* _repository.getCashFlow(startTime, endTime)
-      .map((event) {
-        double income = 0;
-        double expense = 0;
-        event.forEach((element) {
-          if(element.income != null && element.expense != null) {
-            income += element.income.abs();
-            expense += element.expense.abs();
-          }
-        });
-        incomeAndExpenses.add([income, expense]);
-        return CashFlowState(income, expense);
-      })
-    .doOnError((e) {
+    double income = 0;
+    double expense = 0;
+    _repository.getCashFlow(startTime, endTime).map((event) {
+      event.forEach((element) {
+        if (element.income != null && element.expense != null) {
+          income += element.income.abs();
+          expense += element.expense.abs();
+        }
+      });
+    }).doOnError((e) {
       print(e);
     });
+    yield CashFlowState(income, expense);
   }
 }
--- lib/ui/home/HomeState.dart
+++ lib/ui/home/HomeState.dart
@@ -7,7 +7,11 @@ class LoadingState extends HomeState {}
 class CashFlowState extends HomeState {
   double income;
   double expense;
-  CashFlowState(this.income, this.expense);
+  List<double> data = [];
+
+  CashFlowState(this.income, this.expense) {
+    data..add(income)..add(expense);
+  }
 }
 
 class ResetState extends HomeState {}
